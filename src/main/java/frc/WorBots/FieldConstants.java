8bb6ffd75cf1d2ebd101b899757257b4ffb4818d
Raleigh Slack <raleighslack@gmail.com>
2024-01-15 13:48:24 -0500

    Add secondary field

--- src/main/java/frc/WorBots/FieldConstants.java
+++ src/main/java/frc/WorBots/FieldConstants.java
@@ -15,7 +15,7 @@ import frc.WorBots.util.Logger;
 // Units are in meters and radians
 // Some measurements in the field manual are incorrect, reference drawings.
 public class FieldConstants {
-  public static final boolean isWPIField = true;
+  public static final boolean isWPIField = false;
 
   public static final double fieldLength = Units.inchesToMeters(651.25); // 16.451 m
   public static final double fieldWidth = Units.inchesToMeters(323.25); // 8.211 m
@@ -327,5 +327,140 @@ public class FieldConstants {
                   new Rotation3d(
                       new Quaternion(-0.5, 0.0, 0.0,
                           0.8660254037844387))))),
-      fieldLength, fieldWidth) : null;
+      fieldLength, fieldWidth) : new AprilTagFieldLayout(
+      List.of(
+          new AprilTag(1,
+              new Pose3d(
+                  15.079471999999997,
+                  0.24587199999999998,
+                  1.355852,
+                  new Rotation3d(
+                      new Quaternion(0.5, 0.0, 0.0,
+                          0.8660254037844386)))),
+          new AprilTag(2,
+              new Pose3d(
+                  16.185134,
+                  0.883666,
+                  1.355852,
+                  new Rotation3d(
+                      new Quaternion(0.5, 0.0, 0.0,
+                          0.8660254037844386)))),
+          new AprilTag(3,
+              new Pose3d(
+                  16.579342,
+                  Units.inchesToMeters(207 - 22.25),
+                  1.4511020000000001,
+                  new Rotation3d(
+                      new Quaternion(0.0, 0.0, 0.0,
+                          1.0)))),
+          new AprilTag(4,
+              new Pose3d(
+                  16.579342,
+                  Units.inchesToMeters(207),
+                  1.4511020000000001, new Rotation3d(
+                      new Quaternion(0.0, 0.0, 0.0,
+                          1.0)))),
+          new AprilTag(5,
+              new Pose3d(
+                  14.7,
+                  8.2042,
+                  1.355852,
+                  new Rotation3d(
+                      new Quaternion(-0.7071067811865475,
+                          0.0, 0.0,
+                          0.7071067811865476)))),
+          new AprilTag(6,
+              new Pose3d(
+                  1.8415,
+                  8.2042,
+                  1.355852,
+                  new Rotation3d(
+                      new Quaternion(-0.7071067811865475,
+                          0.0, 0.0,
+                          0.7071067811865476)))),
+          new AprilTag(7,
+              new Pose3d(
+                  -0.038099999999999995,
+                  5.547867999999999,
+                  1.4511020000000001,
+                  new Rotation3d(
+                      new Quaternion(1.0, 0.0, 0.0,
+                          0.0)))),
+          new AprilTag(8,
+              new Pose3d(
+                  -0.038099999999999995,
+                  4.982717999999999,
+                  1.4511020000000001,
+                  new Rotation3d(
+                      new Quaternion(1.0, 0.0, 0.0,
+                          0.0)))),
+          new AprilTag(9,
+              new Pose3d(
+                  0.356108,
+                  0.883666,
+                  1.355852,
+                  new Rotation3d(
+                      new Quaternion(0.8660254037844387,
+                          0.0, 0.0,
+                          0.5)))),
+          new AprilTag(10,
+              new Pose3d(
+                  1.4615159999999998,
+                  0.24587199999999998,
+                  1.355852,
+                  new Rotation3d(
+                      new Quaternion(0.8660254037844387,
+                          0.0, 0.0,
+                          0.5)))),
+          new AprilTag(11,
+              new Pose3d(
+                  11.904726,
+                  3.7132259999999997,
+                  1.3208,
+                  new Rotation3d(
+                      new Quaternion(-0.8660254037844387,
+                          0.0, 0.0,
+                          0.5)))),
+          new AprilTag(12,
+              new Pose3d(
+                  11.904726,
+                  4.49834,
+                  1.3208,
+                  new Rotation3d(
+                      new Quaternion(0.8660254037844387,
+                          0.0, 0.0,
+                          0.5)))),
+          new AprilTag(13,
+              new Pose3d(
+                  11.220196,
+                  4.105148,
+                  1.3208,
+                  new Rotation3d(
+                      new Quaternion(0.0, 0.0, 0.0,
+                          1.0)))),
+          new AprilTag(14,
+              new Pose3d(
+                  5.320792,
+                  4.105148,
+                  1.3208,
+                  new Rotation3d(
+                      new Quaternion(1.0, 0.0, 0.0,
+                          0.0)))),
+          new AprilTag(15,
+              new Pose3d(
+                  4.641342,
+                  4.49834,
+                  1.3208,
+                  new Rotation3d(
+                      new Quaternion(0.5, 0.0, 0.0,
+                          0.8660254037844386)))),
+          new AprilTag(16,
+              new Pose3d(
+                  4.641342,
+                  3.7132259999999997,
+                  1.3208,
+                  new Rotation3d(
+                      new Quaternion(-0.5, 0.0, 0.0,
+                          0.8660254037844387))))),
+      fieldLength, fieldWidth);
 }
