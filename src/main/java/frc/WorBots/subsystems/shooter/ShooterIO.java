d158fd90a598b0887f0638ecf93a213477869d64
Raleigh Slack <raleighslack@gmail.com>
2024-01-14 22:11:29 -0500

    Revlib

--- src/main/java/frc/WorBots/subsystems/shooter/ShooterIO.java
+++ src/main/java/frc/WorBots/subsystems/shooter/ShooterIO.java
@@ -2,7 +2,8 @@ package frc.WorBots.subsystems.shooter;
 
 public interface ShooterIO {
     public static class ShooterIOInputs {
-        boolean isConnected = false;
+      boolean isConnected = false;
+      double velocityRPM = 0;
     }
 
     public default void updateInputs(ShooterIOInputs inputs) {
