446015336f2ed081ff9e04cc8f649024ae3f5657
Raleigh Slack <raleighslack@gmail.com>
2024-01-20 16:08:58 -0500

    Basic logic

--- src/main/java/frc/WorBots/subsystems/superstructure/Superstructure.java
+++ src/main/java/frc/WorBots/subsystems/superstructure/Superstructure.java
@@ -1,8 +1,12 @@
 package frc.WorBots.subsystems.superstructure;
 
+import edu.wpi.first.math.VecBuilder;
+import edu.wpi.first.math.Vector;
 import edu.wpi.first.math.controller.ArmFeedforward;
 import edu.wpi.first.math.controller.ElevatorFeedforward;
 import edu.wpi.first.math.controller.ProfiledPIDController;
+import edu.wpi.first.math.numbers.N2;
+import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.WorBots.Constants;
 import frc.WorBots.subsystems.superstructure.SuperstructureIO.SuperstructureIOInputs;
@@ -10,23 +14,42 @@ import frc.WorBots.subsystems.superstructure.SuperstructureIO.SuperstructureIOIn
 public class Superstructure extends SubsystemBase {
   private SuperstructureIO io;
   private SuperstructureIOInputs inputs = new SuperstructureIOInputs();
+  private SuperstructureState state = SuperstructureState.POSE;
+  private Vector<N2> setpoint = VecBuilder.fill(0.0, 0.0);
 
   private ProfiledPIDController pivotController;
   private ProfiledPIDController elevatorController;
   private ElevatorFeedforward elevatorFeedForward;
   private ArmFeedforward pivotFeedForward;
 
+  private enum SuperstructureState {
+    POSE, SHOOTING
+  }
+
   public Superstructure(SuperstructureIO io) {
     this.io = io;
 
     if (!Constants.getSim()) {
-
+      pivotController = new ProfiledPIDController(0, 0, 0, null);
+      elevatorController = new ProfiledPIDController(0, 0, 0, null);
     } else {
-
+      pivotController = new ProfiledPIDController(0, 0, 0, null);
+      elevatorController = new ProfiledPIDController(0, 0, 0, null);
     }
   }
 
   public void periodic() {
     io.updateInputs(inputs);
+    switch (state) {
+      case POSE:
+
+      break;
+      case SHOOTING:
+      break;
+    }
+    if (DriverStation.isDisabled()) {
+      io.setElevatorVoltage(0.0);
+      io.setPivotVoltage(0.0);
+    }
   }
 }
--- src/main/java/frc/WorBots/subsystems/superstructure/SuperstructureIOTalon.java
+++ src/main/java/frc/WorBots/subsystems/superstructure/SuperstructureIOTalon.java
@@ -14,17 +14,34 @@ public class SuperstructureIOTalon implements SuperstructureIO {
   private final DutyCycleEncoder pivotAbsEncoder;
   private final Encoder pivotRelEncoder;
 
+  private static final double elevatorGearing = 1.0; //in meters per revolution
+
   public SuperstructureIOTalon() {
     elevator = new TalonFX(0);
     pivot = new TalonFX(0);
     pivotAbsEncoder = new DutyCycleEncoder(0);
     pivotRelEncoder = new Encoder(0, 0);
-    
+    }
+
+    @Override
+    public void setElevatorVoltage(double volts) {
+      elevator.setVoltage(volts);
+    }
+
+    @Override
+    public void setPivotVoltage(double volts) {
+      pivot.setVoltage(volts);
     }
 
   @Override
   public void updateInputs(SuperstructureIOInputs inputs) {
+    inputs.elevatorPositionMeters = (elevator.getPosition().getValue() / elevatorGearing) * (isElevatorInverted ? 1.0 : -1.0);
+    inputs.elevatorVelocityMetersPerSec = (elevator.getVelocity().getValue() / elevatorGearing) * (isElevatorInverted ? 1.0 : -1.0);
+    inputs.elevatorVoltage = elevator.getMotorVoltage().getValue();
+    inputs.elevatorTemp = elevator.getDeviceTemp().getValue();
 
-    
+    inputs.pivotPositionAbsRad = pivotAbsEncoder.get() * (isPivotInverted ? 1.0 : -1.0);
+    inputs.pivotPositionRelRad = pivotRelEncoder.getDistance() * (isPivotInverted ? 1.0 : -1.0);
+    inputs.pivotVelocityRadPerSec = pivotRelEncoder.getRate() * (isPivotInverted ? 1.0 : -1.0);
   }
 }
