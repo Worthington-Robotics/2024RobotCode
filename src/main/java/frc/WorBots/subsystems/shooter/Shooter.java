ca9a58957a97a46ae942d1d156c601a0633af1ae
Raleigh Slack <raleighslack@gmail.com>
2024-01-20 20:28:03 -0500

    Shooter stuff

--- src/main/java/frc/WorBots/subsystems/shooter/Shooter.java
+++ src/main/java/frc/WorBots/subsystems/shooter/Shooter.java
@@ -1,22 +1,41 @@
 package frc.WorBots.subsystems.shooter;
 
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.controller.SimpleMotorFeedforward;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.WorBots.Constants;
 import frc.WorBots.subsystems.shooter.ShooterIO.ShooterIOInputs;
 import frc.WorBots.util.StatusPage;
 
-public class Shooter extends SubsystemBase {
-    private ShooterIO io;
-    private ShooterIOInputs inputs = new ShooterIOInputs();
+public class Shooter extends SubsystemBase {// 532 rpm/v
+  private ShooterIO io;
+  private ShooterIOInputs inputs = new ShooterIOInputs();
 
-    public Shooter(ShooterIO io) {
-        this.io = io;
+  private PIDController topFlywheelController;
+  private PIDController bottomFlywheelController;
+  private SimpleMotorFeedforward topFlywheelFeedForward;
+  private SimpleMotorFeedforward bottomFlywheelFeedforward;
 
-        StatusPage.reportStatus(StatusPage.SHOOTER_SUBSYSTEM, true);
+  public Shooter(ShooterIO io) {
+    this.io = io;
+
+    if (!Constants.getSim()) {
+      topFlywheelController = new PIDController(0, 0, 0);
+      bottomFlywheelController = new PIDController(0, 0, 0);
+      topFlywheelFeedForward = new SimpleMotorFeedforward(0, 0);
+      bottomFlywheelFeedforward = new SimpleMotorFeedforward(0.0, 0.0);
+    } else {
+      topFlywheelController = new PIDController(0, 0, 0);
+      bottomFlywheelController = new PIDController(0, 0, 0);
+      topFlywheelFeedForward = new SimpleMotorFeedforward(0, 0);
+      bottomFlywheelFeedforward = new SimpleMotorFeedforward(0.0, 0.0);
     }
 
-    @Override
-    public void periodic() {
-        io.updateInputs(inputs);
-        StatusPage.reportStatus(StatusPage.SHOOTER_CONNECTED, inputs.isConnected);
-    }
+    StatusPage.reportStatus(StatusPage.SHOOTER_SUBSYSTEM, true);
+  }
+
+  @Override
+  public void periodic() {
+    io.updateInputs(inputs);
+  }
 }
--- src/main/java/frc/WorBots/subsystems/shooter/ShooterIO.java
+++ src/main/java/frc/WorBots/subsystems/shooter/ShooterIO.java
@@ -1,11 +1,18 @@
 package frc.WorBots.subsystems.shooter;
 
 public interface ShooterIO {
-    public static class ShooterIOInputs {
-      boolean isConnected = false;
-      double velocityRPM = 0;
-    }
+  public static class ShooterIOInputs {
+    boolean isConnected = false;
+    public double velocityRPMTop = 0;
+    public double velocityRPMBottom = 0;
+  }
 
-    public default void updateInputs(ShooterIOInputs inputs) {
-    }
+  public default void setTopFlywheelVolts(double volts) {
+  }
+
+  public default void setBottomFlywheelVolts(double volts) {
+  }
+
+  public default void updateInputs(ShooterIOInputs inputs) {
+  }
 }
