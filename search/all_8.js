1510ab8dfe0c1657d12e475d9feea3b393f2e867
raleighslack <raleighslack@users.noreply.github.com>
2024-01-27 02:23:36 +0000

    deploy: eec22cec80fe97898d003643927eb20f671f4231

--- search/all_8.js
+++ search/all_8.js
@@ -1,10 +1,10 @@
 var searchData=
 [
   ['ideal_5fbattery_0',['IDEAL_BATTERY',['../classfrc_1_1WorBots_1_1util_1_1StatusPage.html#a2b79c0e4aafb1564cafe8430769855b4',1,'frc::WorBots::util::StatusPage']]],
-  ['initialize_1',['initialize',['../classfrc_1_1WorBots_1_1commands_1_1Orbit.html#acae041341612fd79e7197f9d57ea9726',1,'frc.WorBots.commands.Orbit.initialize()'],['../classfrc_1_1WorBots_1_1commands_1_1DriverShoot.html#acfb2630782a3543dd9850cfc78dad09f',1,'frc.WorBots.commands.DriverShoot.initialize()'],['../classfrc_1_1WorBots_1_1commands_1_1DriveToPose.html#a5f1508018ec6be67c07a02dc0a9c39f9',1,'frc.WorBots.commands.DriveToPose.initialize()'],['../classfrc_1_1WorBots_1_1commands_1_1DriveTrajectory.html#a2ae766f21be594e7bbd47a444e7d4f30',1,'frc.WorBots.commands.DriveTrajectory.initialize()']]],
-  ['intake_2',['Intake',['../classfrc_1_1WorBots_1_1subsystems_1_1intake_1_1Intake.html',1,'frc::WorBots::subsystems::intake']]],
-  ['intake_3',['intake',['../classfrc_1_1WorBots_1_1subsystems_1_1intake_1_1Intake.html#a9558724fcb276246786c61ad949fddee',1,'frc::WorBots::subsystems::intake::Intake']]],
-  ['intake_4',['Intake',['../classfrc_1_1WorBots_1_1subsystems_1_1intake_1_1Intake.html#a59bf4682e5d2b8bf058d6dc6ae324709',1,'frc::WorBots::subsystems::intake::Intake']]],
+  ['incrementgamepiece_1',['incrementGamePiece',['../classfrc_1_1WorBots_1_1subsystems_1_1shooter_1_1Shooter.html#ab1a5ac75da62f32a94ceffbf7f70f36a',1,'frc::WorBots::subsystems::shooter::Shooter']]],
+  ['initialize_2',['initialize',['../classfrc_1_1WorBots_1_1commands_1_1Orbit.html#acae041341612fd79e7197f9d57ea9726',1,'frc.WorBots.commands.Orbit.initialize()'],['../classfrc_1_1WorBots_1_1commands_1_1DriverShoot.html#acfb2630782a3543dd9850cfc78dad09f',1,'frc.WorBots.commands.DriverShoot.initialize()'],['../classfrc_1_1WorBots_1_1commands_1_1DriveToPose.html#a5f1508018ec6be67c07a02dc0a9c39f9',1,'frc.WorBots.commands.DriveToPose.initialize()'],['../classfrc_1_1WorBots_1_1commands_1_1DriveTrajectory.html#a2ae766f21be594e7bbd47a444e7d4f30',1,'frc.WorBots.commands.DriveTrajectory.initialize()']]],
+  ['intake_3',['Intake',['../classfrc_1_1WorBots_1_1subsystems_1_1intake_1_1Intake.html',1,'frc.WorBots.subsystems.intake.Intake'],['../classfrc_1_1WorBots_1_1subsystems_1_1intake_1_1Intake.html#a59bf4682e5d2b8bf058d6dc6ae324709',1,'frc.WorBots.subsystems.intake.Intake.Intake(IntakeIO io)']]],
+  ['intake_4',['intake',['../classfrc_1_1WorBots_1_1subsystems_1_1intake_1_1Intake.html#a9558724fcb276246786c61ad949fddee',1,'frc::WorBots::subsystems::intake::Intake']]],
   ['intake_2ejava_5',['Intake.java',['../Intake_8java.html',1,'']]],
   ['intake_5fconnected_6',['INTAKE_CONNECTED',['../classfrc_1_1WorBots_1_1util_1_1StatusPage.html#a6d7f7d92f3289c298588dc15caad3bfd',1,'frc::WorBots::util::StatusPage']]],
   ['intake_5fsubsystem_7',['INTAKE_SUBSYSTEM',['../classfrc_1_1WorBots_1_1util_1_1StatusPage.html#adc37e485e45afa74f0fb8ea507561910',1,'frc::WorBots::util::StatusPage']]],
