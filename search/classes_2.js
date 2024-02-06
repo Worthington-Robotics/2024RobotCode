66d5ad525152f8b000536734f2852e28ff76f292
raleighslack <raleighslack@users.noreply.github.com>
2024-02-06 03:17:36 +0000

    deploy: efa7dca2f66483aeae34556ff036cec12700cf6e

--- search/classes_2.js
+++ search/classes_2.js
@@ -1,8 +1,9 @@
 var searchData=
 [
   ['drive_0',['Drive',['../classfrc_1_1WorBots_1_1subsystems_1_1drive_1_1Drive.html',1,'frc::WorBots::subsystems::drive']]],
-  ['drivershoot_1',['DriverShoot',['../classfrc_1_1WorBots_1_1commands_1_1DriverShoot.html',1,'frc::WorBots::commands']]],
-  ['drivetopose_2',['DriveToPose',['../classfrc_1_1WorBots_1_1commands_1_1DriveToPose.html',1,'frc::WorBots::commands']]],
-  ['drivetrajectory_3',['DriveTrajectory',['../classfrc_1_1WorBots_1_1commands_1_1DriveTrajectory.html',1,'frc::WorBots::commands']]],
-  ['drivewithjoysticks_4',['DriveWithJoysticks',['../classfrc_1_1WorBots_1_1commands_1_1DriveWithJoysticks.html',1,'frc::WorBots::commands']]]
+  ['driverclimb_1',['DriverClimb',['../classfrc_1_1WorBots_1_1commands_1_1DriverClimb.html',1,'frc::WorBots::commands']]],
+  ['drivershoot_2',['DriverShoot',['../classfrc_1_1WorBots_1_1commands_1_1DriverShoot.html',1,'frc::WorBots::commands']]],
+  ['drivetopose_3',['DriveToPose',['../classfrc_1_1WorBots_1_1commands_1_1DriveToPose.html',1,'frc::WorBots::commands']]],
+  ['drivetrajectory_4',['DriveTrajectory',['../classfrc_1_1WorBots_1_1commands_1_1DriveTrajectory.html',1,'frc::WorBots::commands']]],
+  ['drivewithjoysticks_5',['DriveWithJoysticks',['../classfrc_1_1WorBots_1_1commands_1_1DriveWithJoysticks.html',1,'frc::WorBots::commands']]]
 ];
