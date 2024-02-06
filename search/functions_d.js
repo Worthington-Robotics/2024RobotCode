a29b30d3c501ababe1e2fd88437861a247285ecf
lobsterwise <lobsterwise@users.noreply.github.com>
2024-02-06 20:30:29 +0000

    deploy: b7b921f9dcd09db815e24e7666eaa6e07c9eb112

--- search/functions_d.js
+++ search/functions_d.js
@@ -2,13 +2,14 @@ var searchData=
 [
   ['reportmetadata_0',['reportMetadata',['../classfrc_1_1WorBots_1_1util_1_1StatusPage.html#a98bca9216ef078bc94e5ff8c1c91dfa7',1,'frc::WorBots::util::StatusPage']]],
   ['reportstatus_1',['reportStatus',['../classfrc_1_1WorBots_1_1util_1_1StatusPage.html#a5d83a851fba39ff39bbd49431d1e5335',1,'frc::WorBots::util::StatusPage']]],
-  ['resetpose_2',['resetPose',['../classfrc_1_1WorBots_1_1util_1_1PoseEstimator.html#abc13337de05b01965e93a537cc26cd8a',1,'frc::WorBots::util::PoseEstimator']]],
-  ['robotcontainer_3',['RobotContainer',['../classfrc_1_1WorBots_1_1RobotContainer.html#a4a679aeea438710ebf0d0d24417fe25f',1,'frc::WorBots::RobotContainer']]],
-  ['robotinit_4',['robotInit',['../classfrc_1_1WorBots_1_1Robot.html#a73b2963f9c5af74b4732a50fc1443aa9',1,'frc::WorBots::Robot']]],
-  ['robotperiodic_5',['robotPeriodic',['../classfrc_1_1WorBots_1_1Robot.html#ac90c67cbc84575dccfcb0921b8820bf3',1,'frc::WorBots::Robot']]],
-  ['rotationsequence_6',['RotationSequence',['../classfrc_1_1WorBots_1_1util_1_1trajectory_1_1RotationSequence.html#af98159c2d5edd48369542fda2ce42d97',1,'frc::WorBots::util::trajectory::RotationSequence']]],
-  ['rotationtopose_7',['rotationToPose',['../classfrc_1_1WorBots_1_1util_1_1GeomUtil.html#a9d802bc2a2ab17895c23ba1768478632',1,'frc::WorBots::util::GeomUtil']]],
-  ['rotationtotransform_8',['rotationToTransform',['../classfrc_1_1WorBots_1_1util_1_1GeomUtil.html#a1d4ac7f0ed85d561ee64175bb1514011',1,'frc::WorBots::util::GeomUtil']]],
-  ['runstate_9',['runState',['../classfrc_1_1WorBots_1_1subsystems_1_1drive_1_1Module.html#ae9e913b07ee73c0b5cf9e27a65d9bc79',1,'frc::WorBots::subsystems::drive::Module']]],
-  ['runvelocity_10',['runVelocity',['../classfrc_1_1WorBots_1_1subsystems_1_1drive_1_1Drive.html#ad0568920d97a2bfbd3ae1ec61a764e89',1,'frc::WorBots::subsystems::drive::Drive']]]
+  ['resetheading_2',['resetHeading',['../classfrc_1_1WorBots_1_1subsystems_1_1drive_1_1Drive.html#a22f0480915fd39d7bdfc9fc2672bd433',1,'frc.WorBots.subsystems.drive.Drive.resetHeading()'],['../interfacefrc_1_1WorBots_1_1subsystems_1_1drive_1_1GyroIO.html#aeae1a1dae0e69610cb1504b3c0726030',1,'frc.WorBots.subsystems.drive.GyroIO.resetHeading()'],['../classfrc_1_1WorBots_1_1subsystems_1_1drive_1_1GyroIOPigeon2.html#a5842f6832fde38a88e5d3ca50bbfe513',1,'frc.WorBots.subsystems.drive.GyroIOPigeon2.resetHeading()'],['../classfrc_1_1WorBots_1_1subsystems_1_1drive_1_1GyroIOSim.html#a79b0c220d3380cb9dfcb15daa87a6f38',1,'frc.WorBots.subsystems.drive.GyroIOSim.resetHeading()']]],
+  ['resetpose_3',['resetPose',['../classfrc_1_1WorBots_1_1util_1_1PoseEstimator.html#abc13337de05b01965e93a537cc26cd8a',1,'frc::WorBots::util::PoseEstimator']]],
+  ['robotcontainer_4',['RobotContainer',['../classfrc_1_1WorBots_1_1RobotContainer.html#a4a679aeea438710ebf0d0d24417fe25f',1,'frc::WorBots::RobotContainer']]],
+  ['robotinit_5',['robotInit',['../classfrc_1_1WorBots_1_1Robot.html#a73b2963f9c5af74b4732a50fc1443aa9',1,'frc::WorBots::Robot']]],
+  ['robotperiodic_6',['robotPeriodic',['../classfrc_1_1WorBots_1_1Robot.html#ac90c67cbc84575dccfcb0921b8820bf3',1,'frc::WorBots::Robot']]],
+  ['rotationsequence_7',['RotationSequence',['../classfrc_1_1WorBots_1_1util_1_1trajectory_1_1RotationSequence.html#af98159c2d5edd48369542fda2ce42d97',1,'frc::WorBots::util::trajectory::RotationSequence']]],
+  ['rotationtopose_8',['rotationToPose',['../classfrc_1_1WorBots_1_1util_1_1GeomUtil.html#a9d802bc2a2ab17895c23ba1768478632',1,'frc::WorBots::util::GeomUtil']]],
+  ['rotationtotransform_9',['rotationToTransform',['../classfrc_1_1WorBots_1_1util_1_1GeomUtil.html#a1d4ac7f0ed85d561ee64175bb1514011',1,'frc::WorBots::util::GeomUtil']]],
+  ['runstate_10',['runState',['../classfrc_1_1WorBots_1_1subsystems_1_1drive_1_1Module.html#ae9e913b07ee73c0b5cf9e27a65d9bc79',1,'frc::WorBots::subsystems::drive::Module']]],
+  ['runvelocity_11',['runVelocity',['../classfrc_1_1WorBots_1_1subsystems_1_1drive_1_1Drive.html#ad0568920d97a2bfbd3ae1ec61a764e89',1,'frc::WorBots::subsystems::drive::Drive']]]
 ];
