5b1bb977b645a34a56d178e087e6d80da23cdfe5
lobsterwise <lobsterwise@users.noreply.github.com>
2024-02-12 19:45:31 +0000

    deploy: 6e2d6cac596a6525f71c907190952b2180b93574

--- search/variables_4.js
+++ search/variables_4.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['elevator_5fconnected_0',['ELEVATOR_CONNECTED',['../classfrc_1_1WorBots_1_1util_1_1debug_1_1StatusPage.html#acdc5b7d42806d27667ee7a12e27c4de1',1,'frc::WorBots::util::debug::StatusPage']]]
+  ['elevator_5fconnected_0',['ELEVATOR_CONNECTED',['../classfrc_1_1WorBots_1_1util_1_1debug_1_1StatusPage.html#acdc5b7d42806d27667ee7a12e27c4de1',1,'frc::WorBots::util::debug::StatusPage']]],
+  ['elevatormaxmeters_1',['elevatorMaxMeters',['../classfrc_1_1WorBots_1_1subsystems_1_1superstructure_1_1Superstructure.html#aafa3279b5d75ed599863a8ac6d0a50b1',1,'frc::WorBots::subsystems::superstructure::Superstructure']]]
 ];
