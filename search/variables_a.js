3530f1a1446b880a5ba366a373b1ad87f03cd695
raleighslack <raleighslack@users.noreply.github.com>
2024-02-18 01:10:58 +0000

    deploy: 87d50a4a2de20b8da071ca469e77401f5f2c3cdf

--- search/variables_a.js
+++ search/variables_a.js
@@ -2,10 +2,11 @@ var searchData=
 [
   ['manual_0',['MANUAL',['../enumfrc_1_1WorBots_1_1subsystems_1_1superstructure_1_1Superstructure_1_1SuperstructureState.html#a9cc76cdc23a822efd62ef453a161ddc9',1,'frc::WorBots::subsystems::superstructure::Superstructure::SuperstructureState']]],
   ['matchtime_1',['MatchTime',['../enumfrc_1_1WorBots_1_1subsystems_1_1lights_1_1Lights_1_1LightsMode.html#a9c0b0a2baf4ad23da27d9dc9e38a81ca',1,'frc::WorBots::subsystems::lights::Lights::LightsMode']]],
-  ['maxmotortemperature_2',['maxMotorTemperature',['../classfrc_1_1WorBots_1_1util_1_1HardwareUtils.html#a658baa9eae400813a113ae868ca19799',1,'frc::WorBots::util::HardwareUtils']]],
-  ['maxquestions_3',['maxQuestions',['../classfrc_1_1WorBots_1_1AutoSelector.html#a38f8c14b67151a26e585d4bc4cc13d20',1,'frc::WorBots::AutoSelector']]],
-  ['medium_4',['MEDIUM',['../enumfrc_1_1WorBots_1_1util_1_1math_1_1ShooterMath_1_1ShotConfidence.html#a42dae9e948892961d25d704602901b34',1,'frc::WorBots::util::math::ShooterMath::ShotConfidence']]],
-  ['midlinex_5',['midLineX',['../classfrc_1_1WorBots_1_1FieldConstants.html#a184cc8bee57c49acb3f60742c0d3d06f',1,'frc::WorBots::FieldConstants']]],
-  ['midliney_6',['midLineY',['../classfrc_1_1WorBots_1_1FieldConstants.html#a14b9554dfecf859ce4406b032db01b0f',1,'frc::WorBots::FieldConstants']]],
-  ['minimumspeed_7',['minimumSpeed',['../classfrc_1_1WorBots_1_1util_1_1control_1_1DriveController.html#a1679a719278983efbb9d08cf2fffb240',1,'frc::WorBots::util::control::DriveController']]]
+  ['max_5fshooter_5frpm_2',['MAX_SHOOTER_RPM',['../classfrc_1_1WorBots_1_1util_1_1math_1_1ShooterMath.html#aec40a68d606c33aaea406fc1d149c2df',1,'frc::WorBots::util::math::ShooterMath']]],
+  ['maxmotortemperature_3',['maxMotorTemperature',['../classfrc_1_1WorBots_1_1util_1_1HardwareUtils.html#a658baa9eae400813a113ae868ca19799',1,'frc::WorBots::util::HardwareUtils']]],
+  ['maxquestions_4',['maxQuestions',['../classfrc_1_1WorBots_1_1AutoSelector.html#a38f8c14b67151a26e585d4bc4cc13d20',1,'frc::WorBots::AutoSelector']]],
+  ['medium_5',['MEDIUM',['../enumfrc_1_1WorBots_1_1util_1_1math_1_1ShooterMath_1_1ShotConfidence.html#a42dae9e948892961d25d704602901b34',1,'frc::WorBots::util::math::ShooterMath::ShotConfidence']]],
+  ['midlinex_6',['midLineX',['../classfrc_1_1WorBots_1_1FieldConstants.html#a184cc8bee57c49acb3f60742c0d3d06f',1,'frc::WorBots::FieldConstants']]],
+  ['midliney_7',['midLineY',['../classfrc_1_1WorBots_1_1FieldConstants.html#a14b9554dfecf859ce4406b032db01b0f',1,'frc::WorBots::FieldConstants']]],
+  ['minimumspeed_8',['minimumSpeed',['../classfrc_1_1WorBots_1_1util_1_1control_1_1DriveController.html#a1679a719278983efbb9d08cf2fffb240',1,'frc::WorBots::util::control::DriveController']]]
 ];
