100a708c64b76f45a92e6f90bd3b67c00928ea1b
lobsterwise <lobsterwise@users.noreply.github.com>
2024-02-23 13:33:55 +0000

    deploy: 2a8ec1f4bfee2445eda99041834f9e21d2d18d6f

--- search/variables_0.js
+++ search/variables_0.js
@@ -5,6 +5,5 @@ var searchData=
   ['alliance_2',['Alliance',['../enumfrc_1_1WorBots_1_1subsystems_1_1lights_1_1Lights_1_1LightsMode.html#ad8faf79f5ca6068f7615407ed39cfcb9',1,'frc::WorBots::subsystems::lights::Lights::LightsMode']]],
   ['apriltags_3',['aprilTags',['../classfrc_1_1WorBots_1_1FieldConstants.html#a4f09e76c68d6191521f30bcf1ef4c145',1,'frc::WorBots::FieldConstants']]],
   ['apriltagwidth_4',['aprilTagWidth',['../classfrc_1_1WorBots_1_1FieldConstants.html#a6e827234e9c7f3fefc9c3cb6ae7ccebe',1,'frc::WorBots::FieldConstants']]],
-  ['auto_5fchosen_5',['AUTO_CHOSEN',['../classfrc_1_1WorBots_1_1util_1_1debug_1_1StatusPage.html#afc6d26417dd09bd8a0876eadee1e648f',1,'frc::WorBots::util::debug::StatusPage']]],
-  ['auto_5frunning_6',['AUTO_RUNNING',['../classfrc_1_1WorBots_1_1util_1_1debug_1_1StatusPage.html#a2e553e258302afaba4af843767848f12',1,'frc::WorBots::util::debug::StatusPage']]]
+  ['auto_5fchosen_5',['AUTO_CHOSEN',['../classfrc_1_1WorBots_1_1util_1_1debug_1_1StatusPage.html#afc6d26417dd09bd8a0876eadee1e648f',1,'frc::WorBots::util::debug::StatusPage']]]
 ];
