f75792007b79f2b5028f08ff36fbf036177290f8
lobsterwise <lobsterwise@users.noreply.github.com>
2024-02-23 13:48:18 +0000

    deploy: 886df97054ccedb4ab01cb9ee622db6dd99bf0a8

--- search/functions_b.js
+++ search/functions_b.js
@@ -1,5 +1,6 @@
 var searchData=
 [
   ['onepiece_0',['onePiece',['../classfrc_1_1WorBots_1_1commands_1_1AutoCommands.html#a020fe7a623738576f6bd31a7287a7c4b',1,'frc::WorBots::commands::AutoCommands']]],
-  ['optimizestate_1',['optimizeState',['../classfrc_1_1WorBots_1_1subsystems_1_1drive_1_1Module.html#ae0724821f4128b179f3909c1ebf533d7',1,'frc::WorBots::subsystems::drive::Module']]]
+  ['operator_3dnew_20commandxboxcontroller_1',['operator=new CommandXboxController',['../classfrc_1_1WorBots_1_1RobotContainer.html#a2b9e5f5c72da0b2acdc3c225d0877906',1,'frc::WorBots::RobotContainer']]],
+  ['optimizestate_2',['optimizeState',['../classfrc_1_1WorBots_1_1subsystems_1_1drive_1_1Module.html#ae0724821f4128b179f3909c1ebf533d7',1,'frc::WorBots::subsystems::drive::Module']]]
 ];
