39741bb33aa7bc7296b627f2f1e4805bd1109796
lobsterwise <lobsterwise@users.noreply.github.com>
2024-02-25 02:47:00 +0000

    deploy: 4adfc9df574573a6ba86713af4fcfe178ac2ee9b

--- search/variables_1.js
+++ search/variables_1.js
@@ -2,5 +2,6 @@ var searchData=
 [
   ['balanced_0',['Balanced',['../enumfrc_1_1WorBots_1_1util_1_1math_1_1GeneralMath_1_1SnapDirection.html#a57c3709674b772e8c996fcc675086af4',1,'frc::WorBots::util::math::GeneralMath::SnapDirection']]],
   ['battery_1',['BATTERY',['../classfrc_1_1WorBots_1_1util_1_1debug_1_1StatusPage.html#ae45c49192a86d1e39e51c41ab8dd11c0',1,'frc::WorBots::util::debug::StatusPage']]],
-  ['brownout_2',['BROWNOUT',['../classfrc_1_1WorBots_1_1util_1_1debug_1_1StatusPage.html#ae3d5a789e3f575c676769df6643e8d82',1,'frc::WorBots::util::debug::StatusPage']]]
+  ['bounce_2',['Bounce',['../enumfrc_1_1WorBots_1_1subsystems_1_1lights_1_1Lights_1_1LightsMode.html#a7745371349fb876b005b8f74054d4096',1,'frc::WorBots::subsystems::lights::Lights::LightsMode']]],
+  ['brownout_3',['BROWNOUT',['../classfrc_1_1WorBots_1_1util_1_1debug_1_1StatusPage.html#ae3d5a789e3f575c676769df6643e8d82',1,'frc::WorBots::util::debug::StatusPage']]]
 ];
