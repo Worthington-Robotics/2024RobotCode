2dc76dc5ebd7046a2d130df67845240e1fca776b
lobsterwise <lobsterwise@users.noreply.github.com>
2024-03-11 11:40:14 +0000

    deploy: 71f90850c692075d684d81c3f46085eeda46df1b

--- search/functions_5.js
+++ search/functions_5.js
@@ -1,7 +1,8 @@
 var searchData=
 [
-  ['fliprotation_0',['flipRotation',['../classfrc_1_1WorBots_1_1util_1_1math_1_1AllianceFlipUtil.html#a4517155776a20de36ff803a1e23b272c',1,'frc::WorBots::util::math::AllianceFlipUtil']]],
-  ['fourpiecelong_1',['fourPieceLong',['../classfrc_1_1WorBots_1_1auto_1_1Autos.html#aad45a9cd28744ebf2e610819e553d75e',1,'frc::WorBots::auto::Autos']]],
-  ['fromdifferentialpose_2',['fromDifferentialPose',['../classfrc_1_1WorBots_1_1util_1_1trajectory_1_1Waypoint.html#aaf46af49e2ef1d67d4cbd575ab2a2286',1,'frc.WorBots.util.trajectory.Waypoint.fromDifferentialPose(Pose2d pose)'],['../classfrc_1_1WorBots_1_1util_1_1trajectory_1_1Waypoint.html#a0070ea8c35ea8d70fe34c4f06fb2b3e0',1,'frc.WorBots.util.trajectory.Waypoint.fromDifferentialPose(Pose2d pose, Rotation2d holonomicRotation)']]],
-  ['fromholonomicpose_3',['fromHolonomicPose',['../classfrc_1_1WorBots_1_1util_1_1trajectory_1_1Waypoint.html#a8951b68d18719603c47ea994bc605989',1,'frc.WorBots.util.trajectory.Waypoint.fromHolonomicPose(Pose2d pose)'],['../classfrc_1_1WorBots_1_1util_1_1trajectory_1_1Waypoint.html#a093bdd0f45e37ff08fcb58fb9da1f50b',1,'frc.WorBots.util.trajectory.Waypoint.fromHolonomicPose(Pose2d pose, Rotation2d driveRotation)']]]
+  ['fivepiecelong_0',['fivePieceLong',['../classfrc_1_1WorBots_1_1auto_1_1Autos.html#a279ea2eeec52288e6de5e8f2f88a0168',1,'frc::WorBots::auto::Autos']]],
+  ['fliprotation_1',['flipRotation',['../classfrc_1_1WorBots_1_1util_1_1math_1_1AllianceFlipUtil.html#a4517155776a20de36ff803a1e23b272c',1,'frc::WorBots::util::math::AllianceFlipUtil']]],
+  ['fourpiecelong_2',['fourPieceLong',['../classfrc_1_1WorBots_1_1auto_1_1Autos.html#aad45a9cd28744ebf2e610819e553d75e',1,'frc::WorBots::auto::Autos']]],
+  ['fromdifferentialpose_3',['fromDifferentialPose',['../classfrc_1_1WorBots_1_1util_1_1trajectory_1_1Waypoint.html#aaf46af49e2ef1d67d4cbd575ab2a2286',1,'frc.WorBots.util.trajectory.Waypoint.fromDifferentialPose(Pose2d pose)'],['../classfrc_1_1WorBots_1_1util_1_1trajectory_1_1Waypoint.html#a0070ea8c35ea8d70fe34c4f06fb2b3e0',1,'frc.WorBots.util.trajectory.Waypoint.fromDifferentialPose(Pose2d pose, Rotation2d holonomicRotation)']]],
+  ['fromholonomicpose_4',['fromHolonomicPose',['../classfrc_1_1WorBots_1_1util_1_1trajectory_1_1Waypoint.html#a8951b68d18719603c47ea994bc605989',1,'frc.WorBots.util.trajectory.Waypoint.fromHolonomicPose(Pose2d pose)'],['../classfrc_1_1WorBots_1_1util_1_1trajectory_1_1Waypoint.html#a093bdd0f45e37ff08fcb58fb9da1f50b',1,'frc.WorBots.util.trajectory.Waypoint.fromHolonomicPose(Pose2d pose, Rotation2d driveRotation)']]]
 ];
