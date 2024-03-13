7b2582cb3d99db56fbca9ca94d649e26dc0cf975
lobsterwise <pangolin630@gmail.com>
2024-03-13 15:18:15 -0400

    Drive subsystem cleanup

--- src/main/java/frc/WorBots/subsystems/drive/Drive.java
+++ src/main/java/frc/WorBots/subsystems/drive/Drive.java
@@ -13,16 +13,11 @@ import edu.wpi.first.math.kinematics.*;
 import edu.wpi.first.math.util.Units;
 import edu.wpi.first.networktables.*;
 import edu.wpi.first.wpilibj.*;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.WorBots.Constants;
 import frc.WorBots.subsystems.drive.GyroIO.GyroIOInputs;
 import frc.WorBots.util.debug.Logger;
 import frc.WorBots.util.debug.StatusPage;
-import frc.WorBots.util.debug.TunablePIDController.TunablePIDGains;
-import frc.WorBots.util.debug.TunablePIDController.TunableProfiledPIDController;
-import frc.WorBots.util.debug.TunablePIDController.TunableTrapezoidConstraints;
 import frc.WorBots.util.math.PoseEstimator;
 import frc.WorBots.util.math.PoseEstimator.*;
 import java.util.List;
@@ -32,10 +27,18 @@ public class Drive extends SubsystemBase {
   public static final double WHEELBASE = Units.inchesToMeters(10);
 
   private final Module[] modules = new Module[4];
-  private ChassisSpeeds setpoint = new ChassisSpeeds();
-
   private final GyroIO gyroIO;
   private final GyroIOInputs gyroInputs = new GyroIOInputs();
+
+  private final SwerveDriveKinematics kinematics =
+      new SwerveDriveKinematics(getModuleTranslations());
+  private final PoseEstimator poseEstimator =
+      new PoseEstimator(VecBuilder.fill(0.003, 0.003, 0.0002));
+
+  private ChassisSpeeds setpoint = new ChassisSpeeds();
+
+  private Twist2d fieldVelocity = new Twist2d();
+  private Rotation2d lastGyroYaw = new Rotation2d();
   private double[] lastModulePositionsMeters = new double[] {0.0, 0.0, 0.0, 0.0};
   private SwerveModuleState[] lastSetpointStates =
       new SwerveModuleState[] {
@@ -44,26 +47,8 @@ public class Drive extends SubsystemBase {
         new SwerveModuleState(),
         new SwerveModuleState()
       };
-  private Rotation2d lastGyroYaw = new Rotation2d();
-  private final SwerveDriveKinematics kinematics =
-      new SwerveDriveKinematics(getModuleTranslations());
-  private final PoseEstimator poseEstimator =
-      new PoseEstimator(VecBuilder.fill(0.003, 0.003, 0.0002));
+
   private final NetworkTableInstance instance = NetworkTableInstance.getDefault();
-  private Twist2d fieldVelocity = new Twist2d();
-
-  /** Theta setpoint. Can be null */
-  private Rotation2d thetaSetpoint = null;
-
-  /** PID controller for the theta setpoint */
-  private final TunableProfiledPIDController thetaController =
-      new TunableProfiledPIDController(
-          new TunablePIDGains(driveTableName, "Theta Gains"),
-          new TunableTrapezoidConstraints(driveTableName, "Theta Constraints"));
-
-  /** Whether or not to automatically remove the theta setpoint when it is reached */
-  private boolean autoRemoveThetaSetpoint;
-
   private static final String driveTableName = "Drive";
   private final NetworkTable driveTable = instance.getTable(driveTableName);
   private final DoubleArrayPublisher speedSetpointPublisher =
@@ -78,13 +63,13 @@ public class Drive extends SubsystemBase {
       driveTable.getDoubleArrayTopic("Pose Estimator").publish();
 
   /**
-   * The main swerve drive subsystem.
+   * The main swerve drive subsystem
    *
-   * @param gyroIO The IO to use for the gyro.
-   * @param flModule The IO to use for the front left module.
-   * @param frModule The IO to use for the front right module.
-   * @param blModule The IO to use for the back left module.
-   * @param brModule The IO to use for the back right module.
+   * @param gyroIO The IO to use for the gyro
+   * @param flModule The IO to use for the front left module
+   * @param frModule The IO to use for the front right module
+   * @param blModule The IO to use for the back left module
+   * @param brModule The IO to use for the back right module
    */
   public Drive(
       GyroIO gyroIO, ModuleIO flModule, ModuleIO frModule, ModuleIO blModule, ModuleIO brModule) {
@@ -94,24 +79,12 @@ public class Drive extends SubsystemBase {
     modules[2] = new Module(blModule, 2);
     modules[3] = new Module(brModule, 3);
 
-    if (RobotBase.isReal()) {
-      thetaController.setGains(3.9, 0.000, 0.0);
-      thetaController.setConstraints(9.5, 3.6);
-    } else {
-      thetaController.setGains(20.0, 0.08, 0.0);
-      thetaController.setConstraints(3.0, 2.0);
-    }
-    thetaController.pid.enableContinuousInput(0, 2 * Math.PI);
-    thetaController.pid.setTolerance(Units.degreesToRadians(2.5));
-
     StatusPage.reportStatus(StatusPage.DRIVE_SUBSYSTEM, true);
   }
 
   public void periodic() {
     gyroIO.updateInputs(gyroInputs);
     StatusPage.reportStatus(StatusPage.GYROSCOPE, gyroInputs.connected);
-
-    thetaController.update();
 
     for (Module module : modules) {
       module.periodic();
@@ -122,24 +95,10 @@ public class Drive extends SubsystemBase {
         module.stop();
       }
       stop();
-      removeThetaSetpoint();
     } else {
-      double setpointRadsPerSec = setpoint.omegaRadiansPerSecond;
+      final double setpointRadsPerSec = setpoint.omegaRadiansPerSecond;
 
-      // Apply additional rotation to get to theta setpoint
-      SmartDashboard.putBoolean("Drive/Theta Setpoint Exists", thetaSetpoint != null);
-      if (thetaSetpoint != null) {
-        SmartDashboard.putNumber("Drive/Theta Setpoint", thetaSetpoint.getRadians());
-        if (autoRemoveThetaSetpoint && thetaController.pid.atGoal()) {
-          removeThetaSetpoint();
-        } else {
-          final double additional =
-              thetaController.pid.calculate(getRotation().getRadians(), thetaSetpoint.getRadians());
-          SmartDashboard.putNumber("Drive/Theta Additional Demand", additional);
-          setpointRadsPerSec += additional;
-        }
-      }
-
+      // Calculate our desired momentary twist
       final var setpointTwist =
           new Pose2d()
               .log(
@@ -148,6 +107,8 @@ public class Drive extends SubsystemBase {
                           setpoint.vxMetersPerSecond * Constants.ROBOT_PERIOD,
                           setpoint.vyMetersPerSecond * Constants.ROBOT_PERIOD),
                       new Rotation2d(setpointRadsPerSec * Constants.ROBOT_PERIOD)));
+
+      // Convert the twist to ChassisSpeeds in per-second units
       final var adjustedSpeeds =
           new ChassisSpeeds(
               setpointTwist.dx / Constants.ROBOT_PERIOD,
@@ -178,6 +139,7 @@ public class Drive extends SubsystemBase {
       }
     }
 
+    // Get measured states from modules
     SwerveModuleState[] measuredStates = new SwerveModuleState[4];
     for (int i = 0; i < 4; i++) {
       measuredStates[i] = modules[i].getState();
@@ -194,10 +156,11 @@ public class Drive extends SubsystemBase {
       lastModulePositionsMeters[i] = modules[i].getPositionMeters();
     }
 
-    var twist = kinematics.toTwist2d(wheelDeltas);
-    final var gyroYaw = new Rotation2d(gyroInputs.yawPositionRad);
+    final var twist = kinematics.toTwist2d(wheelDeltas);
+    final var gyroYaw = getYaw();
+    // If the gyro is connected, use it's dtheta as it is more accurate
     if (gyroInputs.connected) {
-      twist = new Twist2d(twist.dx, twist.dy, gyroYaw.minus(lastGyroYaw).getRadians());
+      twist.dtheta = gyroYaw.minus(lastGyroYaw).getRadians();
     }
     lastGyroYaw = gyroYaw;
     poseEstimator.addDriveData(Timer.getFPGATimestamp(), twist);
@@ -221,18 +184,18 @@ public class Drive extends SubsystemBase {
   }
 
   /**
-   * Gets the last swerve module states.
+   * Gets the last swerve module states
    *
-   * @return The last measured swerve module states.
+   * @return The last measured swerve module states
    */
   public SwerveModuleState[] getLastSwerveModuleStates() {
     return lastSetpointStates;
   }
 
   /**
-   * Adds vision data to the drive subsystem.
+   * Adds vision data to the drive subsystem
    *
-   * @param updates The vision updates to be added.
+   * @param updates The vision updates to be added
    */
   public void addVisionData(List<TimestampedVisionUpdate> updates) {
     poseEstimator.addVisionData(updates);
@@ -250,36 +213,36 @@ public class Drive extends SubsystemBase {
   }
 
   /**
-   * Returns the maximum linear speed (free speed) that the drive train can physically attain.
+   * Returns the maximum linear speed (free speed) that the drive train can physically attain
    *
-   * @return The value in meters per second.
+   * @return The value in meters per second
    */
   public double getMaxLinearSpeedMetersPerSec() {
     return 4.5;
   }
 
   /**
-   * Gets the current rotation of the drive base.
+   * Gets the current rotation of the drive base
    *
-   * @return The current yaw of the robot.
+   * @return The current yaw of the robot
    */
   public Rotation2d getRotation() {
     return poseEstimator.getLatestPose().getRotation();
   }
 
   /**
-   * Gets the current pose of the robot.
+   * Gets the current pose of the robot
    *
-   * @return The X,Y, and \theta
+   * @return The field-relative robot pose
    */
   public Pose2d getPose() {
     return poseEstimator.getLatestPose();
   }
 
   /**
-   * Gets the current velocity on the field.
+   * Gets the current velocity on the field
    *
-   * @return Returns the velocity as a twist.
+   * @return Returns the velocity as a twist
    */
   public Twist2d getFieldVelocity() {
     return fieldVelocity;
@@ -295,63 +258,63 @@ public class Drive extends SubsystemBase {
   }
 
   /**
-   * Gets the current yaw velocity.
+   * Gets the current yaw velocity
    *
-   * @return The yaw velocity in rads per second.
+   * @return The yaw velocity in rads per second
    */
   public double getYawVelocity() {
     return gyroInputs.yawVelocityRadPerSec;
   }
 
   /**
-   * Gets the current pitch velocity.
+   * Gets the current pitch velocity
    *
-   * @return The pitch velocity in rads per second.
+   * @return The pitch velocity in rads per second
    */
   public double getPitchVelocity() {
     return gyroInputs.pitchVelocityRadPerSec;
   }
 
   /**
-   * Gets the current roll velocity.
+   * Gets the current roll velocity
    *
-   * @return The roll velocity in rads per second.
+   * @return The roll velocity in rads per second
    */
   public double getRollVelocity() {
     return gyroInputs.rollVelocityRadPerSec;
   }
 
   /**
-   * Gets the current pitch.
+   * Gets the current pitch
    *
-   * @return The pitch as a rotation.
+   * @return The pitch as a rotation
    */
   public Rotation2d getPitch() {
     return new Rotation2d(gyroInputs.pitchPositionRad);
   }
 
   /**
-   * Gets the current yaw.
+   * Gets the current yaw
    *
-   * @return The yaw as a rotation.
+   * @return The yaw as a rotation
    */
   public Rotation2d getYaw() {
     return new Rotation2d(gyroInputs.yawPositionRad);
   }
 
   /**
-   * Gets the current roll.
+   * Gets the current roll
    *
-   * @return The roll as a rotation.
+   * @return The roll as a rotation
    */
   public Rotation2d getRoll() {
     return new Rotation2d(gyroInputs.rollPositionRad);
   }
 
   /**
-   * Sets the pose of the pose estimator, used on starting auto.
+   * Sets the pose of the pose estimator, used on starting auto
    *
-   * @param pose The pose to be set.
+   * @param pose The pose to be set
    */
   public void setPose(Pose2d pose) {
     poseEstimator.resetPose(pose);
@@ -363,9 +326,9 @@ public class Drive extends SubsystemBase {
   }
 
   /**
-   * Gets the most recent swerve module positions.
+   * Gets the most recent swerve module positions
    *
-   * @return The positions.
+   * @return The positions
    */
   public SwerveModulePosition[] getLastSwerveModulePositions() {
     return new SwerveModulePosition[] {
@@ -395,20 +358,20 @@ public class Drive extends SubsystemBase {
   }
 
   /**
-   * Runs the provided ChassisSpeeds.
+   * Runs the provided ChassisSpeeds
    *
-   * @param speeds The speeds to be run.
+   * @param speeds The speeds to be run
    */
   public void runVelocity(ChassisSpeeds speeds) {
     setpoint = speeds;
   }
 
-  /** Stops the drive train by clearing the chassis speeds. */
+  /** Stops the drive train by clearing the chassis speeds */
   public void stop() {
     runVelocity(new ChassisSpeeds());
   }
 
-  /** Stops the drive train in an X pattern, locking it in place. */
+  /** Stops the drive train in an X pattern, locking it in place */
   public void stopWithLock() {
     stop();
     for (int i = 0; i < 4; i++) {
@@ -419,57 +382,9 @@ public class Drive extends SubsystemBase {
   }
 
   /**
-   * Sets a continuous theta setpoint that will not clear automatically
+   * Gets the module translations relative to the robot's center
    *
-   * @param setpoint The setpoint
-   */
-  public void setContinuousThetaSetpoint(Rotation2d setpoint) {
-    removeThetaSetpoint();
-    this.thetaSetpoint = setpoint;
-    autoRemoveThetaSetpoint = false;
-  }
-
-  /**
-   * Sets a single theta setpoint that will automatically clear when it is reached
-   *
-   * @param setpoint The setpoint
-   */
-  public void setSingleThetaSetpoint(Rotation2d setpoint) {
-    removeThetaSetpoint();
-    this.thetaSetpoint = setpoint;
-    autoRemoveThetaSetpoint = true;
-  }
-
-  /** Removes the existing theta setpoint */
-  public void removeThetaSetpoint() {
-    this.thetaSetpoint = null;
-    autoRemoveThetaSetpoint = false;
-    thetaController.pid.reset(getRotation().getRadians());
-  }
-
-  /**
-   * Returns a command that will set a single theta setpoint
-   *
-   * @param setpoint The setpoint
-   * @return The command
-   */
-  public Command setSingleThetaSetpointCommand(Rotation2d setpoint) {
-    return this.runOnce(() -> setSingleThetaSetpoint(setpoint));
-  }
-
-  /**
-   * Returns a command that will remove the theta setpoint
-   *
-   * @return The command
-   */
-  public Command removeThetaSetpointCommand() {
-    return this.runOnce(() -> removeThetaSetpoint());
-  }
-
-  /**
-   * Gets the module translation, basically where they are relative to the robots center.
-   *
-   * @return The translations.
+   * @return The translations
    */
   public Translation2d[] getModuleTranslations() {
     return new Translation2d[] {
