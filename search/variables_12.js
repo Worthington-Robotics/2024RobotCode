82b75c56f72b7ec6f39129b429c66df70e0dffdf
lobsterwise <lobsterwise@users.noreply.github.com>
2024-03-20 00:02:22 +0000

    deploy: 54ff501e7fca61d8c6d860487c8a879c63b1ba07

--- search/variables_12.js
+++ search/variables_12.js
@@ -1,5 +1,7 @@
 var searchData=
 [
-  ['wheelbase_0',['WHEELBASE',['../classfrc_1_1WorBots_1_1subsystems_1_1drive_1_1Drive.html#a4331862183e2d82cc57537a394ab469e',1,'frc::WorBots::subsystems::drive::Drive']]],
-  ['winggamepiecelocations_1',['wingGamePieceLocations',['../classfrc_1_1WorBots_1_1auto_1_1AutoUtil.html#a2fac942fb528ad8c0d1b8a7ce9b5a369',1,'frc::WorBots::auto::AutoUtil']]]
+  ['wallsidecenterpoint_0',['wallSideCenterpoint',['../classfrc_1_1WorBots_1_1auto_1_1AutoUtil.html#af365db764c19119056d85f99b24c0563',1,'frc::WorBots::auto::AutoUtil']]],
+  ['wallsidefarshootingpose_1',['wallSideFarShootingPose',['../classfrc_1_1WorBots_1_1auto_1_1AutoUtil.html#a9ed6e4e0180cfc96e9f1a046b5d16068',1,'frc::WorBots::auto::AutoUtil']]],
+  ['wheelbase_2',['WHEELBASE',['../classfrc_1_1WorBots_1_1subsystems_1_1drive_1_1Drive.html#a4331862183e2d82cc57537a394ab469e',1,'frc::WorBots::subsystems::drive::Drive']]],
+  ['winggamepiecelocations_3',['wingGamePieceLocations',['../classfrc_1_1WorBots_1_1auto_1_1AutoUtil.html#a2fac942fb528ad8c0d1b8a7ce9b5a369',1,'frc::WorBots::auto::AutoUtil']]]
 ];
