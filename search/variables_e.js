18d1ccb5e2dd6a2c477590bc0fb725084a464ad7
lobsterwise <lobsterwise@users.noreply.github.com>
2024-03-25 22:25:22 +0000

    deploy: ade18eee74e6a473640ce01e2a97a6bc3a6ebb2f

--- search/variables_e.js
+++ search/variables_e.js
@@ -2,10 +2,11 @@ var searchData=
 [
   ['rainbow_0',['Rainbow',['../enumfrc_1_1WorBots_1_1subsystems_1_1lights_1_1Lights_1_1LightsMode.html#a5ec8e1171d656db28a7336233c4de85f',1,'frc::WorBots::subsystems::lights::Lights::LightsMode']]],
   ['redblue_1',['RedBlue',['../enumfrc_1_1WorBots_1_1subsystems_1_1lights_1_1Lights_1_1LightsMode.html#a87e52c4c88a94da6ff0708368018937b',1,'frc::WorBots::subsystems::lights::Lights::LightsMode']]],
-  ['robot_5fcode_2',['ROBOT_CODE',['../classfrc_1_1WorBots_1_1util_1_1debug_1_1StatusPage.html#aa95c1aa45f4c4d224f31947ea284ed2f',1,'frc::WorBots::util::debug::StatusPage']]],
-  ['robot_5flength_3',['ROBOT_LENGTH',['../classfrc_1_1WorBots_1_1Constants.html#a52cb374c3c91d9f850ca6b3369354c34',1,'frc::WorBots::Constants']]],
-  ['robot_5fperiod_4',['ROBOT_PERIOD',['../classfrc_1_1WorBots_1_1Constants.html#a8d7df0ea01536d83611d153e50f72f78',1,'frc::WorBots::Constants']]],
-  ['robot_5fperiod_5fhz_5',['ROBOT_PERIOD_HZ',['../classfrc_1_1WorBots_1_1Constants.html#a0c8966f0bcd74e69e0273ef9f9de743a',1,'frc::WorBots::Constants']]],
-  ['robot_5fwidth_6',['ROBOT_WIDTH',['../classfrc_1_1WorBots_1_1Constants.html#aee75e4cc04ec3696ab988f04b09c7e6c',1,'frc::WorBots::Constants']]],
-  ['rotational_5fspeed_7',['ROTATIONAL_SPEED',['../classfrc_1_1WorBots_1_1util_1_1control_1_1DriveController.html#aee2f6fe6b0ea6a8b77e4c1aeb78482bc',1,'frc::WorBots::util::control::DriveController']]]
+  ['ripple_2',['Ripple',['../enumfrc_1_1WorBots_1_1subsystems_1_1lights_1_1Lights_1_1LightsMode.html#a30ca3137ac65b20c6f56cb032723bc0d',1,'frc::WorBots::subsystems::lights::Lights::LightsMode']]],
+  ['robot_5fcode_3',['ROBOT_CODE',['../classfrc_1_1WorBots_1_1util_1_1debug_1_1StatusPage.html#aa95c1aa45f4c4d224f31947ea284ed2f',1,'frc::WorBots::util::debug::StatusPage']]],
+  ['robot_5flength_4',['ROBOT_LENGTH',['../classfrc_1_1WorBots_1_1Constants.html#a52cb374c3c91d9f850ca6b3369354c34',1,'frc::WorBots::Constants']]],
+  ['robot_5fperiod_5',['ROBOT_PERIOD',['../classfrc_1_1WorBots_1_1Constants.html#a8d7df0ea01536d83611d153e50f72f78',1,'frc::WorBots::Constants']]],
+  ['robot_5fperiod_5fhz_6',['ROBOT_PERIOD_HZ',['../classfrc_1_1WorBots_1_1Constants.html#a0c8966f0bcd74e69e0273ef9f9de743a',1,'frc::WorBots::Constants']]],
+  ['robot_5fwidth_7',['ROBOT_WIDTH',['../classfrc_1_1WorBots_1_1Constants.html#aee75e4cc04ec3696ab988f04b09c7e6c',1,'frc::WorBots::Constants']]],
+  ['rotational_5fspeed_8',['ROTATIONAL_SPEED',['../classfrc_1_1WorBots_1_1util_1_1control_1_1DriveController.html#aee2f6fe6b0ea6a8b77e4c1aeb78482bc',1,'frc::WorBots::util::control::DriveController']]]
 ];
