64bb2b135cd1b554ae8cd3fd015310e2cc874231
lobsterwise <lobsterwise@users.noreply.github.com>
2024-03-25 03:07:09 +0000

    deploy: a082cd6680d5ad78421ba5129e418393abd6ea5c

--- search/functions_13.js
+++ search/functions_13.js
@@ -1,7 +1,7 @@
 var searchData=
 [
   ['waitfordriverstationbutton_0',['waitForDriverstationButton',['../classfrc_1_1WorBots_1_1util_1_1UtilCommands.html#aaff846ca33f35c1208a58d0b2b84849a',1,'frc::WorBots::util::UtilCommands']]],
-  ['wave_1',['wave',['../classfrc_1_1WorBots_1_1subsystems_1_1lights_1_1LightsUtil.html#ae47b90c0c1d4f3f73699ae783eb9f7f8',1,'frc::WorBots::subsystems::lights::LightsUtil']]],
+  ['wave_1',['wave',['../classfrc_1_1WorBots_1_1subsystems_1_1lights_1_1LightsUtil.html#a8dbca1f26ed2836e4acf5d281a505ce5',1,'frc::WorBots::subsystems::lights::LightsUtil']]],
   ['waypoint_2',['Waypoint',['../classfrc_1_1WorBots_1_1util_1_1trajectory_1_1Waypoint.html#a8b9c2869747fe02f4c40d06c5f741fe8',1,'frc.WorBots.util.trajectory.Waypoint.Waypoint()'],['../classfrc_1_1WorBots_1_1util_1_1trajectory_1_1Waypoint.html#a211a1aa180b5a4b0905061eb55cc16bf',1,'frc.WorBots.util.trajectory.Waypoint.Waypoint(Translation2d translation, Rotation2d driveRotation, Rotation2d holonomicRotation)'],['../classfrc_1_1WorBots_1_1util_1_1trajectory_1_1Waypoint.html#ab3a6e32b001260a52e0aee2220a614a0',1,'frc.WorBots.util.trajectory.Waypoint.Waypoint(Translation2d translation)']]],
   ['wingpass_3',['WingPass',['../classfrc_1_1WorBots_1_1commands_1_1WingPass.html#a3e326b7ca76d3656b6f33277875eef09',1,'frc::WorBots::commands::WingPass']]],
   ['withintolerance_4',['withinTolerance',['../classfrc_1_1WorBots_1_1commands_1_1DriveToPose.html#a580d539596c0241044f0ffdc7b0f73c1',1,'frc::WorBots::commands::DriveToPose']]],
--- search/variables_3.js
+++ search/variables_3.js
@@ -3,14 +3,13 @@ var searchData=
   ['deadband_0',['DEADBAND',['../classfrc_1_1WorBots_1_1util_1_1control_1_1DriveController.html#a8d68f8895b7d3cb682f1f44f2531cb5f',1,'frc::WorBots::util::control::DriveController']]],
   ['delivery_1',['Delivery',['../enumfrc_1_1WorBots_1_1subsystems_1_1lights_1_1Lights_1_1LightsMode.html#a9a8152250183adfd702a7dcd8b8984fb',1,'frc::WorBots::subsystems::lights::Lights::LightsMode']]],
   ['disabled_2',['DISABLED',['../enumfrc_1_1WorBots_1_1subsystems_1_1superstructure_1_1Superstructure_1_1SuperstructureState.html#a9863b83096fd41a4d0ee82ee596cf7df',1,'frc::WorBots::subsystems::superstructure::Superstructure::SuperstructureState']]],
-  ['disabled_3',['Disabled',['../enumfrc_1_1WorBots_1_1subsystems_1_1lights_1_1Lights_1_1LightsMode.html#acff3f4a9be20a0212ca9aa3407c56753',1,'frc::WorBots::subsystems::lights::Lights::LightsMode']]],
-  ['distance_5fthreshold_4',['DISTANCE_THRESHOLD',['../classfrc_1_1WorBots_1_1subsystems_1_1intake_1_1Intake.html#ab14583120f8835931163748dd00ac9c8',1,'frc::WorBots::subsystems::intake::Intake']]],
-  ['drive_5',['drive',['../classfrc_1_1WorBots_1_1RobotContainer.html#a96025f9b1978bda4dbe599cfef5b8aa9',1,'frc::WorBots::RobotContainer']]],
-  ['drive_5fcontroller_6',['DRIVE_CONTROLLER',['../classfrc_1_1WorBots_1_1util_1_1debug_1_1StatusPage.html#a4404be9f2bf3c3723f14cb84f2aab991',1,'frc::WorBots::util::debug::StatusPage']]],
-  ['drive_5fcurve_5famount_7',['DRIVE_CURVE_AMOUNT',['../classfrc_1_1WorBots_1_1util_1_1control_1_1DriveController.html#a44dc363300b8c4c595bebf9ec16e916a',1,'frc::WorBots::util::control::DriveController']]],
-  ['drive_5fspeed_5fmultiplier_8',['DRIVE_SPEED_MULTIPLIER',['../classfrc_1_1WorBots_1_1util_1_1control_1_1DriveController.html#aaa0d42b693fd0c5cd88fdd02d67a52f3',1,'frc::WorBots::util::control::DriveController']]],
-  ['drive_5fsubsystem_9',['DRIVE_SUBSYSTEM',['../classfrc_1_1WorBots_1_1util_1_1debug_1_1StatusPage.html#aa8fac478e1032b857362ad6581e3ffa5',1,'frc::WorBots::util::debug::StatusPage']]],
-  ['driver_10',['driver',['../classfrc_1_1WorBots_1_1RobotContainer.html#a14892d493bd7a1780b0196c7a63fbc3a',1,'frc::WorBots::RobotContainer']]],
-  ['driver_5fcam_11',['DRIVER_CAM',['../classfrc_1_1WorBots_1_1util_1_1debug_1_1StatusPage.html#af12136137cd89924157aeaca4cbc8b07',1,'frc::WorBots::util::debug::StatusPage']]],
-  ['driver_5fstation_12',['DRIVER_STATION',['../classfrc_1_1WorBots_1_1util_1_1debug_1_1StatusPage.html#aa847b0e47dd6c4527ed4247ac0f27cac',1,'frc::WorBots::util::debug::StatusPage']]]
+  ['distance_5fthreshold_3',['DISTANCE_THRESHOLD',['../classfrc_1_1WorBots_1_1subsystems_1_1intake_1_1Intake.html#ab14583120f8835931163748dd00ac9c8',1,'frc::WorBots::subsystems::intake::Intake']]],
+  ['drive_4',['drive',['../classfrc_1_1WorBots_1_1RobotContainer.html#a96025f9b1978bda4dbe599cfef5b8aa9',1,'frc::WorBots::RobotContainer']]],
+  ['drive_5fcontroller_5',['DRIVE_CONTROLLER',['../classfrc_1_1WorBots_1_1util_1_1debug_1_1StatusPage.html#a4404be9f2bf3c3723f14cb84f2aab991',1,'frc::WorBots::util::debug::StatusPage']]],
+  ['drive_5fcurve_5famount_6',['DRIVE_CURVE_AMOUNT',['../classfrc_1_1WorBots_1_1util_1_1control_1_1DriveController.html#a44dc363300b8c4c595bebf9ec16e916a',1,'frc::WorBots::util::control::DriveController']]],
+  ['drive_5fspeed_5fmultiplier_7',['DRIVE_SPEED_MULTIPLIER',['../classfrc_1_1WorBots_1_1util_1_1control_1_1DriveController.html#aaa0d42b693fd0c5cd88fdd02d67a52f3',1,'frc::WorBots::util::control::DriveController']]],
+  ['drive_5fsubsystem_8',['DRIVE_SUBSYSTEM',['../classfrc_1_1WorBots_1_1util_1_1debug_1_1StatusPage.html#aa8fac478e1032b857362ad6581e3ffa5',1,'frc::WorBots::util::debug::StatusPage']]],
+  ['driver_9',['driver',['../classfrc_1_1WorBots_1_1RobotContainer.html#a14892d493bd7a1780b0196c7a63fbc3a',1,'frc::WorBots::RobotContainer']]],
+  ['driver_5fcam_10',['DRIVER_CAM',['../classfrc_1_1WorBots_1_1util_1_1debug_1_1StatusPage.html#af12136137cd89924157aeaca4cbc8b07',1,'frc::WorBots::util::debug::StatusPage']]],
+  ['driver_5fstation_11',['DRIVER_STATION',['../classfrc_1_1WorBots_1_1util_1_1debug_1_1StatusPage.html#aa847b0e47dd6c4527ed4247ac0f27cac',1,'frc::WorBots::util::debug::StatusPage']]]
 ];
