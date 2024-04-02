dc76974f1e6625c60fde4bdfd123eafa96b47149
lobsterwise <lobsterwise@users.noreply.github.com>
2024-04-02 12:59:56 +0000

    deploy: fa3b963973ac3c7759d41cc034da7539a24043a1

--- search/variables_0.js
+++ search/variables_0.js
@@ -3,7 +3,8 @@ var searchData=
   ['all_5fauto_5fquestions_0',['ALL_AUTO_QUESTIONS',['../classfrc_1_1WorBots_1_1util_1_1debug_1_1StatusPage.html#a53e8f21662853a2982b026277397bca3',1,'frc::WorBots::util::debug::StatusPage']]],
   ['all_5fsystems_1',['ALL_SYSTEMS',['../classfrc_1_1WorBots_1_1util_1_1debug_1_1StatusPage.html#a597fc5002acf5ebb0f819752dccc0e4a',1,'frc::WorBots::util::debug::StatusPage']]],
   ['alliance_2',['Alliance',['../enumfrc_1_1WorBots_1_1subsystems_1_1lights_1_1Lights_1_1LightsMode.html#ad8faf79f5ca6068f7615407ed39cfcb9',1,'frc::WorBots::subsystems::lights::Lights::LightsMode']]],
-  ['apriltags_3',['aprilTags',['../classfrc_1_1WorBots_1_1FieldConstants.html#a4f09e76c68d6191521f30bcf1ef4c145',1,'frc::WorBots::FieldConstants']]],
-  ['apriltagwidth_4',['aprilTagWidth',['../classfrc_1_1WorBots_1_1FieldConstants.html#a6e827234e9c7f3fefc9c3cb6ae7ccebe',1,'frc::WorBots::FieldConstants']]],
-  ['auto_5fchosen_5',['AUTO_CHOSEN',['../classfrc_1_1WorBots_1_1util_1_1debug_1_1StatusPage.html#afc6d26417dd09bd8a0876eadee1e648f',1,'frc::WorBots::util::debug::StatusPage']]]
+  ['ampsidecenterpoint_3',['ampSideCenterpoint',['../classfrc_1_1WorBots_1_1auto_1_1AutoUtil.html#ada785ef5ec35eb10adae7d09ffc9fc93',1,'frc::WorBots::auto::AutoUtil']]],
+  ['apriltags_4',['aprilTags',['../classfrc_1_1WorBots_1_1FieldConstants.html#a4f09e76c68d6191521f30bcf1ef4c145',1,'frc::WorBots::FieldConstants']]],
+  ['apriltagwidth_5',['aprilTagWidth',['../classfrc_1_1WorBots_1_1FieldConstants.html#a6e827234e9c7f3fefc9c3cb6ae7ccebe',1,'frc::WorBots::FieldConstants']]],
+  ['auto_5fchosen_6',['AUTO_CHOSEN',['../classfrc_1_1WorBots_1_1util_1_1debug_1_1StatusPage.html#afc6d26417dd09bd8a0876eadee1e648f',1,'frc::WorBots::util::debug::StatusPage']]]
 ];
