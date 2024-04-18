8b96cdcca9fc71a60cbc5d9a54b72b73fdc82495
lobsterwise <pangolin630@gmail.com>
2024-04-18 09:55:10 -0400

    Notify when valid autos are generated

--- src/main/java/frc/WorBots/RobotContainer.java
+++ src/main/java/frc/WorBots/RobotContainer.java
@@ -12,6 +12,7 @@ import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.GenericHID.RumbleType;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.*;
 import edu.wpi.first.wpilibj2.command.button.*;
 import frc.WorBots.auto.AutoSelector;
@@ -178,6 +179,7 @@ public class RobotContainer {
       if (DriverStation.getAlliance().isPresent()) {
         registerAutos();
         validAutosGenerated = true;
+        SmartDashboard.putBoolean("DB/LED 0", true);
       }
     }
   }
